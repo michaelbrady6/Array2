b91d6df354970df0b2972e89d609af406880b3eb
michaelbrady6 <michael.brady108@gmail.com>
2016-09-07 10:21:54 -0600

    Second for loop is not working

--- Array Boot Camp/src/App.java
+++ Array Boot Camp/src/App.java
@@ -7,17 +7,20 @@ public class App {
 		int number = input.nextInt();
 		String[] candies = new String [number];
 		System.out.println(candies.length);
-		for (int i = 0; i<candies.length-1;)
+		for (int i = 0; i<candies.length;)
 		{
+			int sweetNumber = i + 1;
+			System.out.println("What is sweet #" + sweetNumber);
 			String candy = input2.nextLine();
 			candies[i] = "1";
 			i = i+1;
 		}
-		int j;
+		int k = 0;
+		int j = 0;
 		for (j = 0; j < candies.length; j++);
 		{
-			System.out.println(candies[j]);
+			k = j;
+			System.out.println(candies[k]);
 		}
-		
 	}
 }
