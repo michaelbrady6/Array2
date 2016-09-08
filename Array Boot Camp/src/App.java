3eee3942176778712ad5d8cc2df487e084a2b4ac
michaelbrady6 <michael.brady108@gmail.com>
2016-09-08 09:09:58 -0600

    I'm struggling with the third project

--- Array Boot Camp/src/App.java
+++ Array Boot Camp/src/App.java
@@ -1,10 +1,13 @@
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 public class App {
 	public static void main(String[] args) {
-		array1();
+		//array1();
 		//array2();
+		array3();
 	}
 	public static void array1()
 	{
@@ -55,8 +58,38 @@ public class App {
 	}
 	public static void array3()
 	{
-		
+		int num1 = (int) (Math.random()*100);
+//		int num2 = (int) (Math.random()*100);
+//		int num3 = (int) (Math.random()*100);
+//		int num4 = (int) (Math.random()*100);
+//		int num5 = (int) (Math.random()*100);
+		int [] numbers = new int[5];
+		for (int i = 0; i<5; i++)
+			{
+				numbers[i] = num1;
+				num1 = (int) (Math.random()*100);
+//				if (i>0)
+//					{
+//						if(numbers[i] >numbers[i-1])
+//							{
+//								int temp = numbers[i];
+//								numbers[i] = numbers[i-1];
+//								numbers[i-1] = temp;
+//							}
+//					}
+			}
+		Arrays.sort(numbers);
+		Arrays(numbers, Collections.reverseOrder());
+		for (int i:numbers)
+			{
+				System.out.println(i);
+			}
 	}
+	private static void Arrays(int[] numbers, Comparator<Object> reverseOrder)
+		{
+			// TODO Auto-generated method stub
+			
+		}
 	public static void array4()
 	{
 		
